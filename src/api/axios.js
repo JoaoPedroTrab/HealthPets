7df18d7c382fab78d8a858b68101c295001bd581
JoaoPedroTrab <joaotrabachini@gmail.com>
2023-05-23 17:37:32 -0300

    login funcional

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'localhost:3030/api'
+    baseURL: 'http://172.22:3070/'
 });
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,16 +2,33 @@ import React from 'react';
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import './login.css'
+import Axios from "../../api/axios"
 
 function Login() {
-    const [name, setName] = useState("");
+    const [username, setUsername] = useState("");
+    const [senha, setSenha] = useState("");
     const navigate = useNavigate();
-    const handleSubmit = (event) => {
+    const  handleSubmit  = async (event) => {
         event.preventDefault();
-        localStorage.setItem("username", name);
-        localStorage.setItem("cart", JSON.stringify([]));
-        alert(`Logado com o usuário: ${localStorage.getItem("username")}`);
-        navigate('/home');
+        const valores = {
+            senha,
+            username,
+        }
+        const res = await Axios.post('/usuarios/login', valores)
+        try {
+            if (!res.data.error) {
+                console.log("Usuário não encontrado ou senha incorreta.")
+            }
+            else{
+
+                localStorage.setItem("username", username);
+                localStorage.setItem("cart", JSON.stringify([]));
+                alert(`Logado com o usuário: ${localStorage.getItem("username")}`);
+                navigate('/home');
+            }
+        } catch (error) {
+            res.status(500).json({ error: error });
+        }
     }
     return (
         <div className='login'>
@@ -19,11 +36,11 @@ function Login() {
                 <form className='loginForm' onSubmit={handleSubmit}>
                     <label>
                         <b>Usuário:</b>
-                        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
                     </label>
                     <label>
                         <b>Senha:</b>
-                        <input type="password" name="password" />
+                        <input type="password" value={senha} onChange={(e) => setSenha(e.target.value)} />
                     </label>
                     <button type="submit" > <b>Entrar</b> </button>
                 </form>
