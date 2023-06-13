8369a583975446509f4b441a270a4f2867b7596a
JoaoPedroTrab <joaopedrotrab@hotmail.com>
2023-06-13 16:29:39 -0300

    cadastro pet front finalizado

--- src/presentation/components/CadastroPet/CadastroPet.js
+++ src/presentation/components/CadastroPet/CadastroPet.js
@@ -1,14 +1,62 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import './CadastroPet.css'
+import Axios from '../../../infra/api/axios'
+
 
 function CadastroPet() {
-
-    return (
-        <div className='menuContainer'>
-            teste cadastroPet
-        </div>
-    )
-
-}
+        const [nomePet, setNomePet] = useState("");
+        const [racaPet, setRacaPet] = useState("");
+        const [nascimentoPet, setNascimentoPet] = useState("");
+        const [pesoPet, setPesoPet] = useState("");
+        const navigate = useNavigate();
+        async function handleSubmit (e) {
+            e.preventDefault();
+            const valores = {
+                nomePet,
+                racaPet,
+                nascimentoPet,
+                pesoPet
+            }
+            try {
+              const res = await Axios.post('/pet/adicionar', valores);
+              if (!res.data.erro) {
+                alert("Pet cadastrado")
+                navigate('/home');
+              } 
+            } catch (error) {
+                console.error(error);
+                console.log(valores);
+                alert("Insira todos os campos para realizar o cadastro!")
+            }
+        }
+        return (
+            <div className='login'>
+                <div className='loginBox'>
+                <span className="cadastroForm"><b>Cadastro de Pets</b></span>
+                    <form className='loginForm' onSubmit={handleSubmit}>
+                        <label>
+                            <b>Nome do Pet:</b>
+                            <input type="text" value={nomePet} onChange={(e) => setNomePet(e.target.value)} required />
+                        </label>
+                        <label>
+                            <b>Raça do Pet:</b>
+                            <input type="text" value={racaPet} onChange={(e) => setRacaPet(e.target.value)} required />
+                        </label>
+                        <label>
+                            <b>Peso do Pet:</b>
+                            <input type="text" value={pesoPet} onChange={(e) => setPesoPet(e.target.value)} required />
+                        </label>
+                        <label>
+                            <b>Nascimento do Pet:</b>
+                            <input type="text" value={nascimentoPet} onChange={(e) => setNascimentoPet(e.target.value)} required />
+                        </label>
+                        <button type="submit" > <b>Adicionar</b> </button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
 
 export default CadastroPet;
